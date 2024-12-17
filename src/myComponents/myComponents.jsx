9a296cd5e2b5ae5c20d94bbc8d63e19f18d4602f
Surya Annadurai <suryaannadurai1041999@gmail.com>
2024-12-18 00:39:22 +0530

    updated

--- src/myComponents/myComponents.jsx
+++ src/myComponents/myComponents.jsx
@@ -3,39 +3,162 @@ import styles from './myComponents.module.css'
 
 const MyComponents = () =>{
 
+    const quotesData = [
+        {
+            quote :"It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding.",
+            author :"Adam Scott"
+        },
+        {
+            quote :"Eat breakfast like a king, lunch like a prince, and dinner like a pauper.",
+            author :"Adelle Davis"
+        },
+
+        {
+            quote :"We are indeed much more than what we eat, but what we eat can nevertheless help us to be much more than what we are.",
+            author :"Adelle Davis"
+        },
+
+        {
+            quote :"Nothing will benefit human health and increase the chances for survival of life on Earth as much as the evolution to a vegetarian diet.",
+            author :"Albert Einstein"
+        },
+
+        {
+            quote :"Eating is always a decision, nobody forces your hand to pick up food and put it into your mouth.",
+            author :"Alice May Brock"
+        },
+        {
+            quote :`Fat is a barrier, a bellicose statement to others that, to some, justifies hostility in kind. The world says to the fat person, "Your fatness is an affront to me, so we have the right to treat you as offensively as you appear." Fat is not merely viewed as another type of tissue, but as a diagnostic sign, a personal statement, and a measure of personality. Too little fat and we see you as being antisocial, fearful and sexless. Too much fat and we see you as slothful, stupid, and sexually hung up.`,
+            author :"Anthelme Brillat-Savarin"
+        },
+        {
+            quote :"Fat people, it is commonly held, should be punished because they offend our aesthetic sensibilities. They take up too much space on subways, buses, airplanes, and elevators. They consume more than they contribute to society. They become ill and need to be taken care of, or they die early and their families are left unsupported. The only way fat people can gain some acceptance and forgiveness for their crime of overeating is to at least try, or look like they are trying, to lose weight. They must never eat an ice cream cone in public, never be seen eating a normal sized portion of non-diet food!",
+            author :"Calvin Trillin"
+        },
+
+        {
+            quote :"Tomatoes and oregano make it Italian; wine and tarragon make it French. Sour cream makes it Russian; lemon and cinnamon make it Greek. Soy sauce makes it Chinese; garlic makes it good.",
+            author :"Calvin Trillin"
+        },
+
+        {
+            quote :"Tell me what you eat, and I will tell you what you are.",
+            author :"Cyra McFadden"
+        },
+
+        {
+            quote :"Health food makes me sick.",
+            author :"Eike von Repkow"
+        },
+
+        
+        {
+            quote :"The most remarkable thing about my mother is that for thirty years she served the family nothing but leftovers. The original meal has never been found.",
+            author :"Epictetus"
+        },
+
+        
+        {
+            quote :"I no longer prepare food or drink with more than one ingredient.",
+            author :"Epictetus"
+        },
+
+        
+        {
+            quote :"Eating is really one of your indoor sports. You play three times a day, and it's well worth while to make the game as pleasant as possible.",
+            author :"Fran Lebowitz"
+        },
+
+        {
+            quote :"My ability to tolerate shame, to compartmentalize it, to swallow it, increased right along with my belt size. it came with the territory of being heavy. Obese people have a lifetime of experience with shame.",
+            author :"Fran Lebowitz"
+        },
+
+        {
+            quote :"The people who can most successfully lose weight and maintain a healthy life style are foodies. When it comes to healthy eating, people who know how to cook and make ingredients taste good have a distinct advantage over those who can't.",
+            author :"G. K. Chesterton"
+        },
+
+        {
+            quote :"He who comes first, eats first. [Familiar as: First come first served.]",
+            author :"Geoffrey Neighor"
+        },
+        {
+            quote :"Bear in mind that you should conduct yourself in life as at a feast.",
+            author :"George Dennison Prentice"
+        },
+
+        {
+            quote :"I have never cared much for fish - it floats in the belly as much as in the pond.",
+            author :"Henry Bromel"
+        },
+
+        {
+            quote :"If the divine creator has taken pains to give us delicious and exquisite things to eat, the least we can do is prepare them well and serve them with ceremony.",
+            author :"J. R. R. Tolkien"
+        },
+
+        {
+            quote :"Food is an important part of a balanced diet.",
+            author :" "
+        },
+
+        {
+            quote :"My favourite animal is steak.",
+            author :" "
+        },
+
+        {
+            quote :"Music with dinner is an insult both to the cook and the violinist.",
+            author :" "
+        },
+
+        {
+            quote :"Just as animal research tells us that gluttony and sloth are side effects of a drive to accumulate body fat, it also says that eating in moderation and being physically active (literally, having the energy to exercise) is not evidence of moral rectitude. Rather, they're the metabolic benefits of a body that's programmed to remain lean.",
+            author :" "
+        },
+
+        {
+            quote :"Tomatoes and oregano make it Italian; wine and tarragon make it French. Sour cream makes it Russian; lemon and cinnamon make it Greek. Soy sauce makes it Chinese; garlic makes it good.",
+            author :"Calvin Trillin"
+        },
+
+        {
+            quote :"Good food ends with good talk.",
+            author :" "
+        },
+
+        {
+            quote :"There is no love sincerer than the love of food.",
+            author :" "
+        },
+
+        {
+            quote :"What some call health if purchased by perpetual anxiety about diet, isn't much better than tedious disease.",
+            author :" "
+        },
+
+        {
+            quote :"It's important to begin a search on a full stomach.",
+            author :" "
+        },
+
+        {
+            quote :"If more of us valued food and cheer and song above hoarded gold, it would be a merrier world..",
+            author :" "
+        },
+
+
+    ]
+
     return (
         <>
         <div className={styles.com_container}>
-        <Quotes quotes ={"It's amazing how pervasive food is. Every second commercial is for food. Every second TV episode takes place around a meal. In the city, you can't go ten feet without seeing or smelling a restaurant. There are 20 foot high hamburgers up on billboards. I am acutely aware of food, and its omnipresence is astounding."} author={"Adam Scott"} />
-        <Quotes quotes ={"Eat breakfast like a king, lunch like a prince, and dinner like a pauper."} author={"Adelle Davis"} />
-        <Quotes quotes ={"We are indeed much more than what we eat, but what we eat can nevertheless help us to be much more than what we are."} author={"Adelle Davis"} />
-        <Quotes quotes ={"Nothing will benefit human health and increase the chances for survival of life on Earth as much as the evolution to a vegetarian diet."} author={"Albert Einstein"} />
-        <Quotes quotes ={"Eating is always a decision, nobody forces your hand to pick up food and put it into your mouth."} author={"Alice May Brock"} />
-        <Quotes quotes ={`Fat is a barrier, a bellicose statement to others that, to some, justifies hostility in kind. The world says to the fat person, "Your fatness is an affront to me, so we have the right to treat you as offensively as you appear." Fat is not merely viewed as another type of tissue, but as a diagnostic sign, a personal statement, and a measure of personality. Too little fat and we see you as being antisocial, fearful and sexless. Too much fat and we see you as slothful, stupid, and sexually hung up.`} author={"Anthelme Brillat-Savarin"} />
-        <Quotes quotes ={"Fat people, it is commonly held, should be punished because they offend our aesthetic sensibilities. They take up too much space on subways, buses, airplanes, and elevators. They consume more than they contribute to society. They become ill and need to be taken care of, or they die early and their families are left unsupported. The only way fat people can gain some acceptance and forgiveness for their crime of overeating is to at least try, or look like they are trying, to lose weight. They must never eat an ice cream cone in public, never be seen eating a normal sized portion of non-diet food!"} author={"Calvin Trillin"} />
-        <Quotes quotes ={"Tomatoes and oregano make it Italian; wine and tarragon make it French. Sour cream makes it Russian; lemon and cinnamon make it Greek. Soy sauce makes it Chinese; garlic makes it good."} author={"Calvin Trillin"} />
-        <Quotes quotes ={"Tell me what you eat, and I will tell you what you are."} author={"Cyra McFadden"} />
-        <Quotes quotes ={"Health food makes me sick."} author={"Eike von Repkow"} />
-        <Quotes quotes ={"The most remarkable thing about my mother is that for thirty years she served the family nothing but leftovers. The original meal has never been found."} author={"Epictetus"} />
-        <Quotes quotes ={"I no longer prepare food or drink with more than one ingredient."} author={"Epictetus"} />
-        <Quotes quotes ={"Eating is really one of your indoor sports. You play three times a day, and it's well worth while to make the game as pleasant as possible."} author={"Fran Lebowitz"} />
-        <Quotes quotes ={"My ability to tolerate shame, to compartmentalize it, to swallow it, increased right along with my belt size. it came with the territory of being heavy. Obese people have a lifetime of experience with shame."} author={"Fran Lebowitz"} />
-        <Quotes quotes ={"The people who can most successfully lose weight and maintain a healthy life style are foodies. When it comes to healthy eating, people who know how to cook and make ingredients taste good have a distinct advantage over those who can't."} author={"G. K. Chesterton"} />
-        <Quotes quotes ={"He who comes first, eats first. [Familiar as: First come first served.]"} author={"Geoffrey Neighor"} />
-        <Quotes quotes ={"Bear in mind that you should conduct yourself in life as at a feast."} author={"George Dennison Prentice"} />
-        <Quotes quotes ={"I have never cared much for fish - it floats in the belly as much as in the pond."} author={"Henry Bromel"} />
-        <Quotes quotes ={"If the divine creator has taken pains to give us delicious and exquisite things to eat, the least we can do is prepare them well and serve them with ceremony."} author={"J. R. R. Tolkien"} />
-        <Quotes quotes ={"Food is an important part of a balanced diet."} author={" "} />
-        <Quotes quotes ={"My favourite animal is steak."} author={" "} />
-        <Quotes quotes ={"Music with dinner is an insult both to the cook and the violinist."} author={" "} />
-        <Quotes quotes ={"Just as animal research tells us that gluttony and sloth are side effects of a drive to accumulate body fat, it also says that eating in moderation and being physically active (literally, having the energy to exercise) is not evidence of moral rectitude. Rather, they're the metabolic benefits of a body that's programmed to remain lean."} author={" "} />
-        <Quotes quotes ={"We don't get fat because we overeat; we overeat because we're getting fat."} author={" "} />
-        <Quotes quotes ={"Tomatoes and oregano make it Italian; wine and tarragon make it French. Sour cream makes it Russian; lemon and cinnamon make it Greek. Soy sauce makes it Chinese; garlic makes it good."} author={"Calvin Trillin"} />
-        <Quotes quotes ={"Good food ends with good talk."} author={" "} />
-        <Quotes quotes ={"There is no love sincerer than the love of food."} author={" "} />
-        <Quotes quotes ={"What some call health if purchased by perpetual anxiety about diet, isn't much better than tedious disease."} author={""} />
-        <Quotes quotes ={"It's important to begin a search on a full stomach."} author={" "} />
-        <Quotes quotes ={"If more of us valued food and cheer and song above hoarded gold, it would be a merrier world.."} author={" "} />
+           {
+           quotesData.map((val , index) =>{cd 
+            return <Quotes quotes ={val.quote} author ={val.author} key ={index} />
+           })
+           }
         </div>
         </>
     )
